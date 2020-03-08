62f034414953fdfbe5a7e0f9ea8e6ef54f8ee98c
khaledCH23 <khaled.chaffar23@gmail.com>
2020-03-08 12:23:12 +0100

    first-commit

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const initialState = {
+    articles: []
+};
+
+function rootReducer(state = initialState, action) {
+    return state;
+}
+
+export default rootReducer;
